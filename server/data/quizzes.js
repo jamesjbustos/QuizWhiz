ddb9a386c01e0d3979b63df1ec7c7bc957168712
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-10-13 14:02:23 -0400

    Data Model

---
+++ server/data/quizzes.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    title: "General Knowledge",
+    description: "A quiz about various topics.",
+    questions: [
+      {
+        question_text: "What is the capital of France?",
+        answer_a: "Berlin",
+        answer_b: "Madrid",
+        answer_c: "Paris",
+        answer_d: "Lisbon",
+        correct_answer: "C",
+      },
+    ],
+  },
+];
