dcde7fda067b30ce9dc272f69f2063a251bce3e6
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-10-13 23:42:19 -0400

    Add to sample data

--- server/data/quizzes.js
+++ server/data/quizzes.js
@@ -11,6 +11,195 @@ export default [
         answer_d: "Lisbon",
         correct_answer: "C",
       },
+      {
+        question_text: "Who wrote 'Romeo and Juliet'?",
+        answer_a: "Charles Dickens",
+        answer_b: "Jane Austen",
+        answer_c: "William Shakespeare",
+        answer_d: "Homer",
+        correct_answer: "C",
+      },
+    ],
+  },
+  {
+    title: "Science and Nature",
+    description: "Questions related to natural and physical sciences.",
+    questions: [
+      {
+        question_text: "Which planet is closest to the sun?",
+        answer_a: "Earth",
+        answer_b: "Mercury",
+        answer_c: "Venus",
+        answer_d: "Mars",
+        correct_answer: "B",
+      },
+      {
+        question_text: "What is the chemical symbol for water?",
+        answer_a: "HO",
+        answer_b: "H2O",
+        answer_c: "O2",
+        answer_d: "CO2",
+        correct_answer: "B",
+      },
+    ],
+  },
+  {
+    title: "Sports",
+    description: "Trivia about various sports and games.",
+    questions: [
+      {
+        question_text: "Which country hosted the 2016 Summer Olympics?",
+        answer_a: "China",
+        answer_b: "Brazil",
+        answer_c: "Germany",
+        answer_d: "South Africa",
+        correct_answer: "B",
+      },
+      {
+        question_text: "Which sport does Roger Federer play?",
+        answer_a: "Cricket",
+        answer_b: "Tennis",
+        answer_c: "Football",
+        answer_d: "Basketball",
+        correct_answer: "B",
+      },
+    ],
+  },
+  {
+    title: "Movies",
+    description: "Test your knowledge about movies and filmmakers.",
+    questions: [
+      {
+        question_text:
+          "Which movie won the Academy Award for Best Picture in 1994?",
+        answer_a: "Pulp Fiction",
+        answer_b: "The Shawshank Redemption",
+        answer_c: "Forrest Gump",
+        answer_d: "The Lion King",
+        correct_answer: "C",
+      },
+      {
+        question_text: "Who played the character of Jack Dawson in Titanic?",
+        answer_a: "Tom Cruise",
+        answer_b: "Leonardo DiCaprio",
+        answer_c: "Johnny Depp",
+        answer_d: "Brad Pitt",
+        correct_answer: "B",
+      },
+    ],
+  },
+  {
+    title: "Geography",
+    description: "Questions about places and geography.",
+    questions: [
+      {
+        question_text: "Which river is the longest in the world?",
+        answer_a: "Amazon River",
+        answer_b: "Nile River",
+        answer_c: "Mississippi River",
+        answer_d: "Yangtze River",
+        correct_answer: "A",
+      },
+      {
+        question_text: "Which country is known as the Land of the Rising Sun?",
+        answer_a: "China",
+        answer_b: "Japan",
+        answer_c: "Korea",
+        answer_d: "Vietnam",
+        correct_answer: "B",
+      },
+    ],
+  },
+  {
+    title: "History",
+    description: "Explore the past with this historical quiz.",
+    questions: [
+      {
+        question_text: "Who was the first president of the United States?",
+        answer_a: "George Washington",
+        answer_b: "Thomas Jefferson",
+        answer_c: "Abraham Lincoln",
+        answer_d: "Benjamin Franklin",
+        correct_answer: "A",
+      },
+      {
+        question_text: "Which ancient wonder was found in the city of Babylon?",
+        answer_a: "Statue of Zeus",
+        answer_b: "Pyramids of Egypt",
+        answer_c: "Hanging Gardens",
+        answer_d: "Colossus of Rhodes",
+        correct_answer: "C",
+      },
+    ],
+  },
+  {
+    title: "Technology",
+    description: "How well do you know about technology and its history?",
+    questions: [
+      {
+        question_text:
+          "Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
+        answer_a: "Apple",
+        answer_b: "Microsoft",
+        answer_c: "Atari",
+        answer_d: "Commodore",
+        correct_answer: "A",
+      },
+      {
+        question_text:
+          "The Python programming language gets its name from the British comedy group 'Monty Python.'",
+        answer_a: "True",
+        answer_b: "False",
+        answer_c: "",
+        answer_d: "",
+        correct_answer: "A",
+      },
+    ],
+  },
+  {
+    title: "Art and Literature",
+    description:
+      "A journey into the world of artistic expressions and writings.",
+    questions: [
+      {
+        question_text: "Who painted the Mona Lisa?",
+        answer_a: "Vincent Van Gogh",
+        answer_b: "Pablo Picasso",
+        answer_c: "Leonardo da Vinci",
+        answer_d: "Claude Monet",
+        correct_answer: "C",
+      },
+      {
+        question_text: "Who is the author of the Harry Potter series?",
+        answer_a: "J.R.R. Tolkien",
+        answer_b: "J.K. Rowling",
+        answer_c: "George R.R. Martin",
+        answer_d: "Ernest Hemingway",
+        correct_answer: "B",
+      },
+    ],
+  },
+  {
+    title: "Food and Beverage",
+    description:
+      "Test your knowledge about various foods and beverages from around the world.",
+    questions: [
+      {
+        question_text: "What is the main ingredient in guacamole?",
+        answer_a: "Tomato",
+        answer_b: "Pepper",
+        answer_c: "Avocado",
+        answer_d: "Onion",
+        correct_answer: "C",
+      },
+      {
+        question_text: "Which country is the origin of the cocktail Mojito?",
+        answer_a: "Mexico",
+        answer_b: "Cuba",
+        answer_c: "Spain",
+        answer_d: "Portugal",
+        correct_answer: "B",
+      },
     ],
   },
 ];
